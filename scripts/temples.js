3e629d0885c9941dcc4457405d7f0800664b50ac
Jordanedwardlwyaw <jlwaya@byupathwy.edu>
2025-07-07 14:40:22 +0300

    update

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,14 +1,14 @@
-// Footer dynamic date info
+
 document.getElementById('year').textContent = new Date().getFullYear();
 document.getElementById('lastModified').textContent = document.lastModified;
 
-// Hamburger toggle
+
 const hamburger = document.getElementById('hamburger');
 const navMenu = document.getElementById('nav-menu');
 
 hamburger.addEventListener('click', () => {
   navMenu.classList.toggle('show');
-  // Toggle hamburger symbol to 'X' or back
+  
   if (navMenu.classList.contains('show')) {
     hamburger.textContent = '✖';
   } else {
