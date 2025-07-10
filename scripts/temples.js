d7afd98680a5c0328370dd90ca156dcd884afc82
Jordanedwardlwyaw <jlwaya@byupathwy.edu>
2025-07-10 11:55:24 +0300

    update

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,17 +1,12 @@
-
 document.getElementById('year').textContent = new Date().getFullYear();
 document.getElementById('lastModified').textContent = document.lastModified;
-
 
 const hamburger = document.getElementById('hamburger');
 const navMenu = document.getElementById('nav-menu');
 
 hamburger.addEventListener('click', () => {
-  navMenu.classList.toggle('show');
-  
-  if (navMenu.classList.contains('show')) {
-    hamburger.textContent = '✖';
-  } else {
-    hamburger.textContent = '☰';
-  }
+  navMenu.classList.toggle('open');
+  const expanded = navMenu.classList.contains('open');
+  hamburger.textContent = expanded ? '✖' : '☰';
+  hamburger.setAttribute('aria-expanded', expanded);
 });
